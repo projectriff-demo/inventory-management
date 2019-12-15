be052f0c391cee6b0fa7204871c2b8add6b3720d
trisberg <trisberg@pivotal.io>
2019-12-15 18:03:42 -0500

    findBySku and updateBySku

--- src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleRepository.java
+++ src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleRepository.java
@@ -5,12 +5,18 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "article")
 public interface ArticleRepository extends CrudRepository<Article, Long> {
 
+  @Query("SELECT id,sku,name,description,price_in_usd,image_url,quantity FROM article WHERE sku = :sku")
+  Article findBySku(@Param("sku") String sku);
+
   @Modifying
-  @Query("DELETE FROM Article WHERE sku = :sku")
+  @Query("UPDATE article SET quantity = :to WHERE sku = :sku AND quantity = :from")
+  int updateBySku(@Param("sku") String sku, @Param("from") int from, @Param("to") int to);
+
+  @Modifying
+  @Query("DELETE FROM article WHERE sku = :sku")
   void deleteBySku(@Param("sku") String sku);
 }
