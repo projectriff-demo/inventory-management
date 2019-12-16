9238adb73510681a14f0452bfc961859998f0cd1
trisberg <trisberg@pivotal.io>
2019-12-16 14:48:20 -0500

    Hide custom search methods, handle updateQuantityBySku method

--- src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleController.java
+++ src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestController
@@ -16,7 +18,23 @@ public class ArticleController {
     this.articleRepository = articleRepository;
   }
 
-  @DeleteMapping(value = "/article")
+  @PatchMapping(value = "/article/updateQuantityBySku")
+  public ResponseEntity<Article> updateQuantityBySku(@RequestParam("sku") String sku, @RequestBody ArticleQuantityUpdate data) {
+    Article found = this.articleRepository.findBySku(sku);
+    if (found == null) {
+      return ResponseEntity.notFound().build();
+    }
+    int count = this.articleRepository.updateQuantityBySku(sku, data.getCurrentQuantity(), data.getNewQuantity());
+    if (count == 0) {
+      return ResponseEntity.badRequest().build();
+    } else {
+      Article updated = new Article(found.getId(), found.getSku(), found.getName(), found.getDescription(),
+        found.getPriceInUsd(), found.getImageUrl(), data.getNewQuantity());
+      return ResponseEntity.ok(updated);
+    }
+  }
+
+  @DeleteMapping(value = "/article/deleteBySku")
   public ResponseEntity<Article> deleteBySku(@RequestParam("sku") String sku) {
     this.articleRepository.deleteBySku(sku);
     return ResponseEntity.noContent().build();
--- src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleRepository.java
+++ src/main/java/io/projectriff/samples/shopping/inventoryapi/article/ArticleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "article")
 public interface ArticleRepository extends CrudRepository<Article, Long> {
@@ -12,10 +13,12 @@ public interface ArticleRepository extends CrudRepository<Article, Long> {
   @Query("SELECT id,sku,name,description,price_in_usd,image_url,quantity FROM article WHERE sku = :sku")
   Article findBySku(@Param("sku") String sku);
 
+  @RestResource(exported = false)
   @Modifying
   @Query("UPDATE article SET quantity = :to WHERE sku = :sku AND quantity = :from")
-  int updateBySku(@Param("sku") String sku, @Param("from") int from, @Param("to") int to);
+  int updateQuantityBySku(@Param("sku") String sku, @Param("from") int from, @Param("to") int to);
 
+  @RestResource(exported = false)
   @Modifying
   @Query("DELETE FROM article WHERE sku = :sku")
   void deleteBySku(@Param("sku") String sku);
